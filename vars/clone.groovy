bd004497aafc8d5d0a82e2de98da8915bf8925b8
Sagar Choudhary <166942461+Sagar1602@users.noreply.github.com>
2026-05-18 12:11:53 +0530

    Add clone function to fetch code from GitHub

---
+++ vars/clone.groovy
@@ -0,0 +1,5 @@
+def call(String GitUrl, String GitBranch){
+  echo "in this step the code is fetched from the Github"
+  git url: "{GitUrl}" , branch: "${GitBranch}"
+  echo "Code clonned successfully"
+}
