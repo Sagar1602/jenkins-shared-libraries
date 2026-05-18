d4ffacc926d61001456857179f8d691971f4d9f4
Sagar Choudhary <166942461+Sagar1602@users.noreply.github.com>
2026-05-18 12:49:18 +0530

    Implement Docker image push functionality
    Add a Jenkins pipeline step to push Docker images to Docker Hub.

---
+++ vars/docker_push.groovy
@@ -0,0 +1,14 @@
+def call(String Project, String ImageTag, String Dockerhub_user){
+  echo "in this step we are pushig the image to dockerhub"
+  withCredentials([usernamePassword(
+    credentialsId:"Dockerhub_cred",
+    passwordVariable:"Dockerhub_pass",
+    usernameVariable:"Dockerhub_user"
+                )])
+  {
+                sh "docker login -u ${Dockerhub_user} -p ${Dockerhub_pass}"
+              //  sh "docker image tag node-app:latest ${env.Dockerhub_user}/node-app:latest"
+                sh "docker push ${Dockerhub_user}/${Project}:${ImageTag}"
+                }
+  
+}
