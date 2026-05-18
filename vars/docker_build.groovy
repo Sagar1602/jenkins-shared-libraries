990dcf037f07cad6a85555cdbc6ff0697d1f4fed
Sagar Choudhary <166942461+Sagar1602@users.noreply.github.com>
2026-05-18 12:29:44 +0530

    Fix Docker build command syntax for image tagging

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser){
   echo "in this step the code is build "
-  sh "docker build-t ${DockerHubUser}/${ProjectName}/${ImageTag} ."
+  sh "docker build-t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
   
 }
