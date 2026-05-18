9c1b62f3029394449ff9b023703ad9ccb390216c
Sagar Choudhary <166942461+Sagar1602@users.noreply.github.com>
2026-05-18 12:12:49 +0530

    Update success message in clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String GitUrl, String GitBranch){
   echo "in this step the code is fetched from the Github"
   git url: "{GitUrl}" , branch: "${GitBranch}"
-  echo "Code clonned successfully"
+  echo "Code clonned successfully :)"
 }
